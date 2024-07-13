a6abe50b2db4bdbe91b041a769b049d0901249c8
SupunSynotec <supun.synotec@gmail.com>
2024-07-13 13:47:50 +0530

    Public Site

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,48 @@
-@extends('frontend.layouts.master')
-@section('title', 'Login')
-@section('styles')
-@endsection
-@section('content')
+<x-guest-layout>
+    <x-authentication-card>
+        <x-slot name="logo">
+            <x-authentication-card-logo />
+        </x-slot>
 
-    <div class="bg-light py-3">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12 mb-0"><a href="index.html">Home</a> <span class="mx-2 mb-0">/</span> <strong
-                        class="text-black">Login</strong></div>
+        <x-validation-errors class="mb-4" />
+
+        @session('status')
+            <div class="mb-4 font-medium text-sm text-green-600">
+                {{ $value }}
             </div>
-        </div>
-    </div>
+        @endsession
 
-    <div class="product_section layout_padding">
-        <div class="container">
-            <div class="col-lg-7 mx-auto">
-                @if (session('status'))
-                    <div class="alert alert-success">
-                        {{ session('status') }}
-                    </div>
-                @endif
-                @if (session('message'))
-                    <div class="alert alert-danger">{{ session('message') }}</div>
-                @endif
-                @if ($errors->any())
-                    <div class="alert alert-danger">
-                        <div class="font-weight-bold">{{ __('Whoops! Something went wrong.') }}</div>
-                        <ul class="list-unstyled mb-0">
-                            @foreach ($errors->all() as $error)
-                                <li class="">{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                @endif
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
 
-                <form method="POST" action="{{ route('login') }}" class="card">
-                    @csrf
-                    <div class="card-body">
-                        <div>
-                            <x-label for="email" value="{{ __('Email') }}" />
-                            <x-input id="email" class="block mt-1 w-full" type="email" name="email"
-                                :value="old('email')" required autofocus autocomplete="username" />
-                        </div>
-
-                        <div class="mt-4">
-                            <x-label for="password" value="{{ __('Password') }}" />
-                            <x-input id="password" class="block mt-1 w-full" type="password" name="password" required
-                                autocomplete="current-password" />
-                        </div>
-
-                        <div class="block mt-4">
-                            <label for="remember_me" class="flex items-center">
-                                <x-checkbox id="remember_me" name="remember" class="mb-0" />
-                                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                            </label>
-                        </div>
-
-                        <div class="flex items-center justify-end mt-4">
-                            <x-button class="ms-4">
-                                {{ __('Log in') }}
-                            </x-button>
-                        </div>
-                    </div>
-                </form>
+            <div>
+                <x-label for="email" value="{{ __('Email') }}" />
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             </div>
-        </div>
-    </div>
 
-@endsection
-@section('scripts')
-@endsection
+            <div class="mt-4">
+                <x-label for="password" value="{{ __('Password') }}" />
+                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+            </div>
+
+            <div class="block mt-4">
+                <label for="remember_me" class="flex items-center">
+                    <x-checkbox id="remember_me" name="remember" />
+                    <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                </label>
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                @if (Route::has('password.request'))
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                        {{ __('Forgot your password?') }}
+                    </a>
+                @endif
+
+                <x-button class="ms-4">
+                    {{ __('Log in') }}
+                </x-button>
+            </div>
+        </form>
+    </x-authentication-card>
+</x-guest-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,17 @@
 <?php
 
-use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Route;
 
-// FRONTEND
-Route::get('/', [FrontendController::class, 'index'])->name('/');
-Route::get('/products', [FrontendController::class, 'products'])->name('products');
-Route::get('/product-details', [FrontendController::class, 'productDetails'])->name('productDetails');
-Route::get('/cart', [FrontendController::class, 'cart'])->name('cart');
-Route::get('/checkout', [FrontendController::class, 'checkout'])->name('checkout');
+Route::get('/', function () {
+    return view('welcome');
+});
 
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
